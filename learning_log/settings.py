632e75a44cd37adf405fef5fce60c359e743eb30
ogunkapc <ogunkapc@gmail.com>
2022-10-03 17:30:49 +0100

    updated .gitignore file and hid django secret key

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -19,8 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+import os
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i(ca*3#w#y)lh!7ueyd$#-z1w)m8@%#tk3smpd#=28-#d^-05+'
+# SECRET_KEY = 'django-insecure-i(ca*3#w#y)lh!7ueyd$#-z1w)m8@%#tk3smpd#=28-#d^-05+'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
