7535ae8844e2c316eff6a5efaf12cdadf550ea3b
ogunkapc <ogunkapc@gmail.com>
2022-10-10 21:56:00 +0100

    created logic to add new entries

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponseRedirect
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 from django.urls import reverse
 from django.shortcuts import render
 from .models import Topic
@@ -34,10 +34,30 @@ def new_topic(request):
         form = TopicForm()
     else:
         # POST data submitted; process data.
-        form = TopicForm(request.POST)
+        form = TopicForm(data=request.POST)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect(reverse('learning_logs:topics'))
 
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    """Add a new entry for a particular topic."""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # No data submitted; Create a blank form.
+        form = EntryForm()
+    else:
+        # POST data submitted; process data.
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
+
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
