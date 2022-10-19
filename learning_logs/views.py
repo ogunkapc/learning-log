9576d0af34ffcd588b8cd67446c3e8e0efa33f3b
ogunkapc <ogunkapc@gmail.com>
2022-10-19 15:16:35 +0100

    created logic to edit existing entries

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponseRedirect
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 from django.urls import reverse
 from django.shortcuts import render
-from .models import Topic
 
 # Create your views here.
 
@@ -61,3 +61,22 @@ def new_entry(request, topic_id):
 
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
+
+
+def edit_entry(request, entry_id):
+    """Edit an existing entry."""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        # Initial request; pre-fill form with the current entry.
+        form = EntryForm(instance=entry)
+    else:
+        # POST data submitted; process data.
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic.id]))
+
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
